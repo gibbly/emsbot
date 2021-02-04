b5d8fa47541c6a7af602b42d59c92530e7c637a6
gibbly <aflilneen@gmail.com>
2021-02-04 01:57:33 -0600

    bring github up to date with the rewrite

--- start.py
+++ start.py
@@ -14,94 +14,150 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 USER = os.getenv('user')
 PASS = os.getenv('pass')
-guildid = int(os.getenv('DISCORD_GUILD'))
-channelid = int(os.getenv('DISCORD_CHANNEL'))
+
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='+', intents = intents)
-laststreamer = ''
-ping = os.getenv('PING_ROLE_ID')
 botid = int(os.getenv('BOT_ID'))
+bot = commands.Bot(command_prefix='+', intents = intents)
 
 
 
+default = {
+"usericon": "https://cdn.discordapp.com/embed/avatars/0.png",
+"nicklink": "",
+"color": 0x1e2936,
+"overridediscord": False,
+"streamthumbnail": "https://cdn.discordapp.com/attachments/758341897453437049/758342103779901440/EMS.png",
+"streamdescription": "no u",
+"extraimage": "",
+}
 
 
 
-
-
-
-async def updatenowplaying():
-
+async def activeget():
     try:
+        err = "the state could not be fetched"
         state = requests.get('https://ems.isitdoneyet.co.uk/api/key/active', auth=(USER, PASS))
+        err = str(state)
         state = json.loads(state.text)
         if 'discord_id' in state:
             pass
         else:
             state["discord_id"] = 0
-
+        return state
     except:
-        print("there was an error fetching the current state" + str(state))
+        print("there was an error fetching the current state" + err)
+        return{"nick": "error" + err}
 
-    if int(state.get('discord_id')) != 0 :
+
+async def userdataget(discord_id):
+    return {"streamdescription": "Custom descriptions coming soon :tm:"}
+
+async def generatecard(user):
+    trashvalue = {}
+    trashvalue.update(default)
+    trashvalue.update(user)
+    user = trashvalue
+    user.update(await userdataget(user.get("discord_id")))
+    embed = discord.Embed(title="", url="", description=user.get("streamdescription"), colour=discord.Colour(user.get("color")))
+    embed.set_image(url=user.get("extraimage"))
+    embed.set_thumbnail(url=user.get("streamthumbnail"))
+    if user.get("overridediscord") == "TRUE":
+        embed.set_author(name=user.get("nick"), url=user.get("nicklink"), icon_url=user.get("usericon"))
+    else:
         try:
-            member = guild.get_member(int(state['discord_id']))
+            member = guild.get_member(int(user.get("discord_id")))
             name = member.display_name
             usericon = member.avatar_url_as(format=None, static_format="jpg")
             usericon = str(str(usericon).split('?')[0])
         except:
-            name = state['nick']
-            usericon = ''
-            print(str(state) + "invalid discord id")
-    else:
-        name = state['nick']
-        usericon = ''
-        print(name + ' has no discord id')
+            name = user.get("nick")
+            usericon = user.get("usericon")
+            print(str(user) + "discord user data scrape failed? falling back to nick")
+        embed.set_author(name=name, url=user.get("nicklink"), icon_url=usericon)
+    return embed
 
-    customdescription = "Custom descriptions coming soon :tm:"
-    streamthumbnail = 'https://cdn.discordapp.com/attachments/758341897453437049/758342103779901440/EMS.png'
-    customurl = ''
 
-    global laststreamer
-    if name != laststreamer:
-        try:
-            if state.get("priority") >= 10:
-                embed=discord.Embed(title="watch " + name + " LIVE NOW!", url="https://www.youtube.com/channel/UCJZX0lhyQyicb9nVDS8R40w/live", description=customdescription)
-                embed.set_author(name=name, url=customurl,icon_url=usericon)
-                embed.set_thumbnail(url=streamthumbnail)
-                embed.set_footer(text="brought to you by ems ;)")
-                await channel.send(embed=embed)
-                await channel.send('<@&' + str(ping) + '>')
-                channel2 = bot.get_channel(756541590494904321)
-                await channel2.send(embed=embed)
+async def streamercard(user):
+    embed = await generatecard(user)
+    embed.add_field(name="Watch on", value="[**[Twitch]**](http://twitch.tv/earthmodularsociety) [**[YouTube]**](http://youtube.com/c/EarthModularSociety/live)")
+    embed.add_field(name="Don't forget to check out the site!", value="http://earthmodularsociety.com/", inline=False)
+    embed.set_footer(text="Brought to you by EMS ;)", icon_url="https://cdn.discordapp.com/attachments/758341897453437049/758342103779901440/EMS.png")
+    return embed
+
+
+
+
+
+
+
+
+
+
+@bot.event
+async def on_message(message):
+    if message.webhook_id != None:
+        if message.content == "blorp":
+            user = await activeget()
+            embed = await streamercard(user)
+            if user.get("priority") >= 10:
+                content = "Come watch " + user.get("nick") + " LIVE " + str(ping.mention)
             else:
-                embed=discord.Embed(title="Now playing: " + name, url="https://www.youtube.com/channel/UCJZX0lhyQyicb9nVDS8R40w/live", description=customdescription)
-                embed.set_author(name= name, url=customurl,icon_url=usericon)
-                embed.set_thumbnail(url=streamthumbnail)
-                embed.set_footer(text="brought to you by ems ;)")
-                await channel.send(embed=embed)
-                channel2 = bot.get_channel(756541590494904321)
-                await channel2.send(embed=embed)
-            laststreamer = name
-        except:
-            pass
+                content = ""
+            await channel.send(embed=embed, content=content)
+
+#        elif message.content[0] == "+":
+#            if message.content.startswith["+nowplaying"]:
+#                await nowplaying()
+#
+#
+#
+#
+#async def card(ctx, *, arg):
+#
+#    arg = arg[2:-1]
+#    user = {"discord_id": arg}
+#    embed = await generatecard(user)
+#    await channel.send(embed=embed)
+#
+#
+#async def nowplaying():
+#    user = await activeget()
+#    embed = await streamercard(user)
+#    if user.get("priority") >= 10:
+#        content = "Come watch " + user.get("nick") + " LIVE " + str(ping.mention)
+#    else:
+#        content = ""
+#    await channel.send(embed=embed, content=content)
+#
+#
+#async def ping(ctx):
+#	print("ping'd")
+#	await ctx.channel.send("pong")
+#
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+
     global guild
-    guild = bot.get_guild(guildid)
+    guild = int(os.getenv('DISCORD_GUILD'))
+    guild = bot.get_guild(guild)
     print(guild)
+
     global channel
-    channel = bot.get_channel(channelid)
-    while True:
-        try:
-            await updatenowplaying()
-        except:
-            await asyncio.sleep(60)
-        await asyncio.sleep(30)
+    channel = int(os.getenv('DISCORD_CHANNEL'))
+    channel = bot.get_channel(channel)
+    print(channel)
+
+    global ping
+    ping = int(os.getenv('PING_ROLE_ID'))
+    ping = guild.get_role(ping)
+    print(ping)
+
+
 
 
 
